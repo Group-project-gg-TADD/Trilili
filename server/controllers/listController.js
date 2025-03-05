81d721b5bbefcaf18181eff3e9dbadf9e11ea001
dianekapr <dianekapratiwi72@gmail.com>
2025-03-05 10:07:28 +0700

    updating list endpoint

---
+++ server/controllers/listController.js
@@ -0,0 +1,21 @@
+const {List, Board} = require("../models")
+
+class listController{
+    static async getList(req, res, next){
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async addList(req, res, next){
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+module.exports = listController
